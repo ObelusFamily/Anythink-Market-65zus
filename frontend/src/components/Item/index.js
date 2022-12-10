dd5c9f63143a3d0a37da903b435a27cfb8aba745
InevitableAnup <anup.borde@earlysalary.com>
2022-12-10 12:10:39 +0530

    Copies placeholder.png to the imgs folder and added condition to render placholder if image not included in payload. Also added destructuring in a few places.

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -8,6 +8,7 @@ import {
   ITEM_PAGE_LOADED,
   ITEM_PAGE_UNLOADED,
 } from "../../constants/actionTypes";
+import placholderImg from '../../imgs/placeholder.png';
 
 const mapStateToProps = (state) => ({
   ...state.item,
@@ -31,34 +32,36 @@ class Item extends React.Component {
   }
 
   render() {
-    if (!this.props.item) {
+    const {currentUser,item, comments, commentErrors, match}=this.props;
+    if (!item) {
       return null;
     }
 
     const markup = {
-      __html: marked(this.props.item.description, { sanitize: true }),
+      __html: marked(item.description, { sanitize: true }),
     };
+    
     const canModify =
-      this.props.currentUser &&
-      this.props.currentUser.username === this.props.item.seller.username;
+      currentUser &&
+      currentUser.username === item.seller.username;
     return (
       <div className="container page" id="item-container">
         <div className="text-dark">
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image}
-                alt={this.props.item.title}
+                src={item.image && item.image !== ''?item.image:placholderImg}
+                alt={item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
               />
             </div>
 
             <div className="col-6">
-              <h1 id="card-title">{this.props.item.title}</h1>
-              <ItemMeta item={this.props.item} canModify={canModify} />
+              <h1 id="card-title">{item.title}</h1>
+              <ItemMeta item={item} canModify={canModify} />
               <div dangerouslySetInnerHTML={markup}></div>
-              {this.props.item.tagList.map((tag) => {
+              {item.tagList.map((tag) => {
                 return (
                   <span className="badge badge-secondary p-2 mx-1" key={tag}>
                     {tag}
@@ -70,10 +73,10 @@ class Item extends React.Component {
 
           <div className="row bg-light-gray p-4">
             <CommentContainer
-              comments={this.props.comments || []}
-              errors={this.props.commentErrors}
-              slug={this.props.match.params.id}
-              currentUser={this.props.currentUser}
+              comments={comments || []}
+              errors={commentErrors}
+              slug={match.params.id}
+              currentUser={currentUser}
             />
           </div>
         </div>
